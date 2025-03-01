16e50031a1ca10a9d11f4ec2f20ca08c2d92b07b
mkarpu <mallireddy97k@gmail.com>
2025-02-28 23:08:07 -0600

    Initial commit

---
+++ ReverseString/NewProject/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public void Add(int a, int b) {
+        System.out.println("Addition is " +(a+b));
+    }
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.Add(5,5);
+    }
+}
