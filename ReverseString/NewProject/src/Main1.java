615a45711577f02c23d549c1fadff6b5ceaba79f
mkarpu <mallireddy97k@gmail.com>
2025-04-21 16:37:14 -0500

    Delete ReverseString/NewProject directory

--- ReverseString/NewProject/src/Main1.java
+++
@@ -1,9 +0,0 @@
-public class Main1 {
-    public int sub(int a, int b) {
-        return a - b;
-    }
-    public static void main(String[] args) {
-        Main1 obj = new Main1();
-        System.out.println("Sub is " +obj.sub(5, 5));
-    }
-}
